0d7405d9107815fbb443ea6a1552afbfa282ba9b
Forever-CodingNoob <johnnylin6256@gmail.com>
2021-07-02 08:36:59 +0800

    fix gp

--- app.py
+++ app.py
@@ -33,24 +33,28 @@ if app.env == 'development':
 
     load_dotenv(dotenv_path=os.path.join(os.getcwd(), '.env'), override=True)
 
-
-
 dstr = lambda s: '>' + str(s) + '<'
 errorstr = lambda s: '[error] ' + str(s)
 
 
 class Question:
 
-    def __init__(self, *, id=None, text, solved=False, userid, groupid, created_time, solved_time=None):
-        self.id=id
+    def __init__(self, *, id=None, text, solved=False, userid, groupid, created_time=None, solved_time=None):
+        self.id = id
         self.text = text
         self.solved = solved
         self.userid = userid
         self.groupid = groupid
-        self.created_time = created_time
+        self.created_time = created_time if created_time is not None else datetime.datetime.now().astimezone(
+            tz=pytz.utc)
         self.solved_time = solved_time
 
         # self.id = int(id) if id is not None else int(getGroup(self.groupid).get().to_dict()['id_next'])
+
+    def set_solved(self):
+        self.solved = True
+        self.solved_time = datetime.datetime.now().astimezone(tz=pytz.utc)
+        self.update()
 
     @staticmethod
     def from_dict(dic, *, groupid, set_id=None):
@@ -78,48 +82,47 @@ class Question:
     @classmethod
     def load_all(cls, groupid, unsolved=False):
         qs = getGroup(groupid).collection('questions')
-        stream=qs.stream() if not unsolved else qs.where('solved','==',False).stream()
+        stream = qs.stream() if not unsolved else qs.where('solved', '==', False).stream()
 
-        return [cls.from_dict(q.to_dict(), groupid=groupid,set_id=q.id) for q in stream]
+        return [cls.from_dict(q.to_dict(), groupid=groupid, set_id=q.id) for q in stream]
 
     @classmethod
     def load(cls, *, id, groupid):
-        q = getGroup(groupid,addGroupIfNotFound=False).collection('questions').document(str(id)).get()
+        q = getGroup(groupid, addGroupIfNotFound=False).collection('questions').document(str(id)).get()
         if not q.exists:
             raise FetchError()
-        return cls.from_dict(q.to_dict(), groupid=groupid,set_id=q.id)
+        return cls.from_dict(q.to_dict(), groupid=groupid, set_id=q.id)
 
     def update(self):
-        q=getGroup(self.groupid).collection('questions').document(str(self.id))
+        q = getGroup(self.groupid).collection('questions').document(str(self.id))
         if q.get().exists:
             q.update(self.to_dict())
         else:
             raise FetchError('question not found; hence it cannot be updated')
 
     def save(self):
-        gp=getGroup(self.groupid)
+        gp = getGroup(self.groupid)
         if self.id is None:
             '''save as new question if id is not given'''
-            newid=gp.get().to_dict()['id_next']
-            self.id=newid
-            gp.update({'id_next':newid+1})
+            newid = gp.get().to_dict()['id_next']
+            self.id = newid
+            gp.update({'id_next': newid + 1})
         gp.collection('questions').document(str(self.id)).set(self.to_dict())
 
     @staticmethod
-    def deleteById(groupid,id):
-        q=getGroup(groupid).collection('questions').document(str(id))
+    def deleteById(groupid, id):
+        q = getGroup(groupid).collection('questions').document(str(id))
         if q.get().exists:
             q.delete()
             return True
         return False
 
-
     def __str__(self):
-        username=line_bot_api.get_profile(user_id=self.userid).display_name
+        username = line_bot_api.get_profile(user_id=self.userid).display_name
         return f'{self.id}. "{self.text}"--from[{username}]'
 
 
-def getGroup(groupid,addGroupIfNotFound=True):
+def getGroup(groupid, addGroupIfNotFound=True):
     gp = db.collection('questions').document(groupid)
     if not gp.get().exists:
         if addGroupIfNotFound:
@@ -127,15 +130,16 @@ def getGroup(groupid,addGroupIfNotFound=True):
         else:
             raise FetchError()
     return gp
+
+
 def addGroup(groupid):
-    gp=db.collection('questions').document(groupid)
+    gp = db.collection('questions').document(groupid)
     if gp.get().exists:
         return
     gp.set({
-        'id_next':0
+        'id_next': 0
     })
     print(f'group "{groupid}" is created')
-
 
 
 class FetchError(Exception):
@@ -180,62 +184,61 @@ def handle_message(event):
     print('*' * 40)
     print('[使用者傳入文字訊息]')
     print(str(event))
+    print(event.message.text)
+    print(event.source.type)
 
     user_msg = event.message.text.strip()
     reply = None
 
-    if user_msg.startswith('?') and len(user_msg) > 1 and event.source.group_id:
+    if (user_msg.startswith('?') or user_msg.startswith('？')) and len(user_msg) > 1 and event.source.type=='group':
         command = user_msg[1:].lstrip()
-        q=Question(
+        q = Question(
             text=command,
-            userid= event.source.user_id,
-            groupid= event.source.group_id,
-            created_time=datetime.datetime.now().astimezone(tz=pytz.utc)
+            userid=event.source.user_id,
+            groupid=event.source.group_id
         )
         q.save()
         reply = TextSendMessage(text=dstr(f"question [{q.id}] is saved"))
     elif user_msg.startswith('/') and len(user_msg) > 1:
         command = user_msg[1:].split()
-        if groupid:=event.source.group_id:
-            if command[0].lower()=='all':
-                qs=Question.load_all(groupid)
-                if not len(qs):
-                    reply = TextSendMessage(text=dstr("(empty)"))
-                else:
-                    reply = TextSendMessage(text=dstr("\n"+"\n".join([str(q) for q in qs])+"\n"))
-            elif command[0].lower()=='uns':
-                qs=Question.load_all(groupid,unsolved=True)
+        if groupid := event.source.group_id:
+            if command[0].lower() == 'all':
+                qs = Question.load_all(groupid)
                 if not len(qs):
                     reply = TextSendMessage(text=dstr("(empty)"))
                 else:
                     reply = TextSendMessage(text=dstr("\n" + "\n".join([str(q) for q in qs]) + "\n"))
-            elif command[0].lower()=='del':
+            elif command[0].lower() == 'uns':
+                qs = Question.load_all(groupid, unsolved=True)
+                if not len(qs):
+                    reply = TextSendMessage(text=dstr("(empty)"))
+                else:
+                    reply = TextSendMessage(text=dstr("\n" + "\n".join([str(q) for q in qs]) + "\n"))
+            elif command[0].lower() == 'del':
                 try:
-                    num=int(command[1])
+                    num = int(command[1])
                 except IndexError:
-                    reply=TextSendMessage(text=errorstr("'/del' missing required argument 'id'"))
+                    reply = TextSendMessage(text=errorstr("'/del' missing required argument 'id'"))
                 except ValueError:
                     reply = TextSendMessage(text=errorstr("the first argument of '/del' must be an integer or 0"))
                 else:
-                    if Question.deleteById(groupid,num):
+                    if Question.deleteById(groupid, num):
                         reply = TextSendMessage(text=dstr(f"question [{num}] is deleted"))
-            elif command[0].lower()=='s':
+            elif command[0].lower() == 's':
                 try:
-                    num=int(command[1])
+                    num = int(command[1])
                 except IndexError:
-                    reply=TextSendMessage(text=errorstr("'/del' missing required argument 'id'"))
+                    reply = TextSendMessage(text=errorstr("'/del' missing required argument 'id'"))
                 except ValueError:
                     reply = TextSendMessage(text=errorstr("the first argument of '/del' must be an integer or 0"))
                 else:
                     try:
-                        q=Question.load(id=num,groupid=groupid)
+                        q = Question.load(id=num, groupid=groupid)
                     except FetchError:
                         reply = TextSendMessage(text=errorstr(f"question [{num}] is not found"))
                     else:
-                        q.solved=True
-                        q.update()
+                        q.set_solved()
                         reply = TextSendMessage(text=dstr(f"question [{num}] is solved"))
-
 
     # 回傳訊息
     if reply:
